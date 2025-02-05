24be60b396559fd314b8a7a2ced99e1013189269
TynaiS <tynai.s.95@gmail.com>
2025-02-05 16:53:12 +0600

    initial commit

---
+++ src/main/java/com/example/schedule_composer/ScheduleComposerApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedule_composer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleComposerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleComposerApplication.class, args);
+	}
+
+}
