72a176c34621107ee8ada41ffaf81bd671c5d370
TynaiS <tynai.s.95@gmail.com>
2025-03-04 00:23:57 +0600

    rename group course teacher endpoint

--- src/main/java/com/example/schedule_composer/utils/ApiConstants.java
+++ src/main/java/com/example/schedule_composer/utils/ApiConstants.java
@@ -3,7 +3,7 @@ package com.example.schedule_composer.utils;
 public class ApiConstants {
     public static final String BASE_API = "/api/v1";
     public static final String COURSE_API = BASE_API + "/courses";
-    public static final String COURSE_GROUP_TEACHER_API = BASE_API + "/course_group_teacher";
+    public static final String COURSE_GROUP_TEACHER_API = BASE_API + "/group_course_teacher";
     public static final String GROUP_API = BASE_API + "/groups";
     public static final String ROOM_API = BASE_API + "/rooms";
     public static final String SCHEDULE_API = BASE_API + "/schedule";
