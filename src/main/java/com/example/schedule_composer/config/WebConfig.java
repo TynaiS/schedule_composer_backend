8dbead7fc2ffe92cbcee65c3a72848f03d696eeb
TynaiS <tynai.s.95@gmail.com>
2025-03-20 16:50:02 +0600

    allow all origins in WebConfig

--- src/main/java/com/example/schedule_composer/config/WebConfig.java
+++ src/main/java/com/example/schedule_composer/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/v1/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE")
                         .allowCredentials(true);
             }
