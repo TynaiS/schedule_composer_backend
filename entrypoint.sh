d240c448bc6aada619715953d65f70e04d4f005a
TormentedTraveler <114169086+TormentedTraveler@users.noreply.github.com>
2025-05-13 14:23:56 +0600

    updated entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,7 @@
 # Create log directory if it doesn't exist
 mkdir -p /logs
 
+touch /logs/app.log
+
 # Run the app and redirect stdout and stderr to a file
 exec java -jar /app.jar > /logs/app.log 2>&1
