0c77f0a7a25073fc47d0f72f0e8a8f7eae7ac444
TynaiS <tynai.s.95@gmail.com>
2025-05-20 16:54:25 +0600

    remove username field from RegisterDTOPost

--- src/main/java/com/example/schedule_composer/dto/post/RegisterDTOPost.java
+++ src/main/java/com/example/schedule_composer/dto/post/RegisterDTOPost.java
@@ -1,6 +1,7 @@
 package com.example.schedule_composer.dto.post;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,8 +14,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegisterDTOPost {
 
+    @NotBlank(message = "Name cannot be blank")
     private String name;
-    private String username;
+
     @Email(message = "Email should be valid")
     private String email;
 
