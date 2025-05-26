6208e1598821f215a56606d158c7dedac0c3cc95
TormentedTraveler <esyashaba@gmail.com>
2025-05-26 17:40:44 +0600

    editer cors settings

--- src/main/java/com/example/schedule_composer/config/SecurityConfig.java
+++ src/main/java/com/example/schedule_composer/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.schedule_composer.security.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -28,6 +29,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(
                                 "/api/v1/auth/**",
                                 "/api/v1/forgot-password/**",
